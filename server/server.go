5374041e4cc3ffb3c7cd32f250e951be3b9e4982
Hieu <dqhieu01@gmail.com>
2021-11-12 15:42:46 +0700

    Add logging, recovery middleware

--- server/server.go
+++ server/server.go
@@ -8,6 +8,15 @@ import (
 
 func Run() {
 	r := gin.Default()
+
+	// Global middleware
+	// Logger middleware will write the logs to gin.DefaultWriter even if you set with GIN_MODE=release.
+	// By default gin.DefaultWriter = os.Stdout
+	r.Use(gin.Logger())
+
+	// Recovery middleware recovers from any panics and writes a 500 if there was one.
+	r.Use(gin.Recovery())
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
